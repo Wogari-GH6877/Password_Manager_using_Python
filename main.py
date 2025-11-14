107cfabf05b7a9359ce4017fa365adfb74e6e166
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-11-14 02:02:12 -0800

    the completed project

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+password_list = []
+
+
+
+password_letter=[random.choice(letters) for _ in range(random.randint(8, 10))]
+password_symbols=[random.choice(symbols) for _ in range(random.randint(2, 4))]
+password_numbers=[random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+
+
+
+password_list=password_letter + password_symbols + password_numbers
+
+random.shuffle(password_list)
+
+mypassword ="".join(password_list)
+
 
 
 def save():
     website = input1.get()
     email = input2.get()
     password = input3.get()
-    with open("file.txt","a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        input1.delete(0,END)
-        input3.delete(0, END)
+
+    is_empty=(len(website)==0 or len(email)==0 or len(password)==0)
+
+    if(is_empty):
+        messagebox.showinfo(title="Oops",message="there is empty field fill it ")
+    else:
+
+
+        is_right=messagebox.askokcancel(title=website,message=f"this are the details \n Website: {website}\n Email: {email} \n Password: {password}")
+
+        if is_right:
+            with open("file.txt","a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                input1.delete(0,END)
+                input3.delete(0, END)
+
+def generate():
+    input3.insert(0,mypassword)
 
 window=Tk()
 # window.minsize(height=400,width=600)
@@ -46,7 +85,7 @@ input3.grid(row=3,column=1)
 
 #Buttons
 
-generate_button=Button(text="Generate Password")
+generate_button=Button(text="Generate Password",command=generate)
 generate_button.grid(column=2,row=3)
 
 add_button=Button(text="Add",width=35,command=save)
